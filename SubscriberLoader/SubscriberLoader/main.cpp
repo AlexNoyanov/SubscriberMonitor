fbabcf0fd351396e9a7a1a4944f9b6707dc8c25f
Alex Noyanov <alex.noyanov@gmail.com>
2019-10-19 15:07:20 +0300

    Some tests for downloading data from database and show it on webpage added. Added page with "login" fields to connect to the database

---
+++ SubscriberLoader/SubscriberLoader/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  SubscriberLoader
+//
+//  Created by Alex Noyanov on 19.10.19.
+//  Copyright © 2019 Popoff Developer Studio. All rights reserved.
+//
+
+// This programm should load Subscribers to the Database
+/*
+ 
+  __
+ (_    |_  _  _  __ o |_  _  __ _ |   _  _  _| _  __
+ __)|_||_)_> (_  |  | |_)(/_ | _> |__(_)(_|(_|(/_ |
+ 
+ */
+
+// To 
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    
+    
+    return 0;
+}
